85b3af67e8451e0f7d0e040446f908b6c17d89ee
Quốc Huy <contactforwork.quochuy@gmail.com>
2026-05-01 23:39:34 +0700

    Add production Firebase web config
    Set Firebase project keys in the frontend config so deployed pages can connect to Authentication and Firestore.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 // Update values from your Firebase project settings.
 // If left blank, the app will continue using localStorage only.
 window.MMO_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDulyaXYt93wv_4jNPI_gIYRAVX5kz5KCM",
+  authDomain: "mmoqh-428da.firebaseapp.com",
+  projectId: "mmoqh-428da",
+  storageBucket: "mmoqh-428da.firebasestorage.app",
+  messagingSenderId: "871458183809",
+  appId: "1:871458183809:web:df151ec6ddf8a50215cb80"
 };
